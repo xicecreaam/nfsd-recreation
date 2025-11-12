c54f436c3746d4ad687ba6950c4cbdf2f865c09f
cataresk 😇 <codqg@icecream.home>
2025-11-12 17:51:38 +0100

    Updated index.js formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ export default function Home() {
       title="Welcome to Need for SpeedDocs"
       description="This website is aiming to rehost speedrunning documentation for most Need for Speed games that has been shut down by Balathruin himself, creator of the original website. Currently looking for contributors to expand.d."
     >
-      <main style={{ padding: '2rem', maxWidth: '900px', textAlign: 'left' }}>
+      <main style={{ padding: '2rem', maxWidth: '2000px', textAlign: 'left' }}>
         <header>
-          <h1 style={{ fontSize: '2.5rem', fontWeight: '700' }}>
+          <h1 style={{ fontSize: '60px', fontWeight: '700' }}>
             Welcome to Need for SpeedDocs
           </h1>
-          <p style={{ marginTop: '0.5rem', fontSize: '1.1rem', color: '#ccc' }}>
+          <p style={{ marginTop: '0.5rem', fontSize: '18px', color: '#ccc' }}>
             This website is aiming to rehost speedrunning documentation for most Need for Speed games that has been shut down by Balathruin himself, creator of the original website.
             Currently looking for contributors to expand.
           </p>
@@ -25,32 +25,38 @@ export default function Home() {
           </h2>
           <ul style={{ listStyleType: 'none', padding: 0, marginTop: '1rem' }}>
             <li style={{ marginBottom: '0.5rem' }}>
-              <Link to="#" style={{ color: '#f55' }}>
-                Need for Speed: Underground
-              </Link>{' '}
-              by <strong>LokyZen (FG)</strong>, <strong>Scorpion (DT & FWD DS)</strong> and{' '}
-              <strong style={{ color: '#c33' }}>GTB (RWD DS)</strong>
+              <Link to="#" style={{ color: '#f55' }}>Need for Speed: Underground 1</Link>{' '}
+              by
+              <Link to="#" style={{ color: '#f55' }}>LokyZen </Link>{' '}
+               (FG), 
+              <Link to="#" style={{ color: '#f55' }}>Scorpion</Link>{' '}
+                (DT & FWD DS) and
+               <Link to="#" style={{ color: '#f55' }}>GTB</Link>{' '}
+                (RWD DS)
             </li>
             <li style={{ marginBottom: '0.5rem' }}>
-              <Link to="#" style={{ color: '#f55' }}>
-                Need for Speed: Most Wanted (2005)
-              </Link>
+              <Link to="#" style={{ color: '#f55' }}>Need for Speed: Underground 2</Link>{' '}
             </li>
             <li style={{ marginBottom: '0.5rem' }}>
-              <Link to="#" style={{ color: '#f55' }}>
-                Need for Speed: Carbon
-              </Link>{' '}
-              by <strong>Artacer</strong> and <strong>Cousin</strong>
+              <Link to="#" style={{ color: '#f55' }}>Need for Speed: Most Wanted (2005)</Link>{' '}
+              by
+              <Link to="#" style={{ color: '#f55' }}>SlivenKage</Link>{' '}
             </li>
             <li style={{ marginBottom: '0.5rem' }}>
-              <Link to="#" style={{ color: '#f55' }}>
-                Need for Speed: Shift
-              </Link>
+              <Link to="#" style={{ color: '#f55' }}>Need for Speed: Carbon</Link>{' '}
+              by
+              <Link to="#" style={{ color: '#f55' }}>Artacer</Link>{' '}
+              and 
+              <Link to="#" style={{ color: '#f55' }}>Cousin</Link>{' '}
             </li>
             <li style={{ marginBottom: '0.5rem' }}>
-              <Link to="#" style={{ color: '#f55' }}>
-                Shift 2: Unleashed
-              </Link>
+              <Link to="#" style={{ color: '#f55' }}>Need for Speed: ProStreet</Link>{' '}
+            </li>
+            <li style={{ marginBottom: '0.5rem' }}>
+              <Link to="#" style={{ color: '#f55' }}>Need for Speed: Undercover </Link>{' '}
+            </li>
+            <li style={{ marginBottom: '0.5rem' }}>
+              <Link to="#" style={{ color: '#f55' }}>Need for Speed: The Run</Link>{' '}
             </li>
           </ul>
         </section>
